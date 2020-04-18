c013401734fc2f62f7969b7e751b5e5308c42c24
ryann254 <keddelyronjoz@gmail.com>
2020-04-18 17:40:58 +0300

    minor change

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,6 +1,4 @@
 import React, { Fragment } from "react";
-import { Link } from "react-router-dom";
-import Github from "../../public/images/icons/github-icon-dark.svg";
 
 function Homepage() {
   return (
